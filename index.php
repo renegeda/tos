eeb33f3b46013a431a240e436ee379c027bf5b58
renegeda <54241294+renegeda@users.noreply.github.com>
2025-04-19 00:15:57 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+require 'includes/db.php';
+require 'includes/header.php';
+?>
 
+<main>
+    <?php include 'includes/main_content.php'; ?>
+</main>
+
+<?php
+require 'includes/footer.php';
+?>
